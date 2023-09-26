1180edb7bcabf90e794869de06c07f9ae5da3f06
CodeSandbox <support@codesandbox.io>
2023-09-26 08:20:14 +0000

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -30,20 +30,6 @@ const init = () => {
             <cropper-handle action="se-resize"></cropper-handle>
             <cropper-handle action="sw-resize"></cropper-handle>
           </cropper-selection>
-           <!--选区2-->
-          <cropper-selection class="selection" x="180" y="5" width="200" height="50" initial-coverage="0"  resizable zoomable multiple>
-            <cropper-grid role="grid" bordered covered></cropper-grid>
-            <cropper-crosshair theme-color="rgba(238, 238, 238, 0.5)" centered></cropper-crosshair>
-            <cropper-handle class="inner" action="move" theme-color="rgba(255, 255, 255, 0.15)"></cropper-handle>
-            <cropper-handle action="n-resize"></cropper-handle>
-            <cropper-handle action="e-resize"></cropper-handle>
-            <cropper-handle action="s-resize"></cropper-handle>
-            <cropper-handle action="w-resize"></cropper-handle>
-            <cropper-handle action="ne-resize"></cropper-handle>
-            <cropper-handle action="nw-resize"></cropper-handle>
-            <cropper-handle action="se-resize"></cropper-handle>
-            <cropper-handle action="sw-resize"></cropper-handle>
-          </cropper-selection>
         </cropper-canvas>`,
   });
 };
